6991ab974eaa7508c5073fe612260a7ea97b5f56
Neras4 <solovbeloz@gmail.com>
2024-02-25 21:56:38 +0600

    Revert "v:0.1"
    This reverts commit 243100223cd9a7ba7c5fad5801146940b6e6e629.

--- src/main/java/com/neras4/book_app/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neras4.book_app.repository;
-
-import com.neras4.book_app.model.Book;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends CrudRepository<Book, Integer> {
-}
